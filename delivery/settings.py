f9067a027384af9d10975b4b8cc507b25f188caa
Your Name <your@example.com>
2021-01-28 14:16:08 +0600

    remover databases

--- delivery/settings.py
+++ delivery/settings.py
@@ -86,16 +86,16 @@ WSGI_APPLICATION = 'delivery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'delivery',
-        'USER': 'deliveryman',
-        'PASSWORD': 'Makerspython05',
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'delivery',
+#         'USER': 'deliveryman',
+#         'PASSWORD': 'Makerspython05',
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
 
 
 DATABASES = {
