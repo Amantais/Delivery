43f28265b587784ed9a79fbbd52e8ab1157c9d88
Your Name <your@example.com>
2021-01-28 15:07:16 +0600

    first commit

--- delivery/settings.py
+++ delivery/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'delivery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'delivery',
