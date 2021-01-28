dc3a028d44be413edf45869c41d07c2069feb6fc
Your Name <your@example.com>
2021-01-28 13:31:50 +0600

    Deploy Heroku

--- delivery/settings.py
+++ delivery/settings.py
@@ -96,12 +96,14 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+
+    
+}
 
 
 # Password validation
